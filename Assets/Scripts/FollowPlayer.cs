974e7c644004018ebd9432cfc70340932b9c9542
caden hl <62642278+khaitick@users.noreply.github.com>
2021-06-02 22:33:38 +0800

    Add content

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public Camera cam1;
+    public Camera cam2;
+    private Vector3 offset = new Vector3(0, 5f, -7f);
+
+    private void Start()
+    {
+        cam1.enabled = true;
+        cam2.enabled = false;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            cam1.enabled = !cam1.enabled;
+            cam2.enabled = !cam2.enabled;
+        }
+
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float carSpeed = 20.0f;
+    public float turnSpeed = 40.0f;
+    public string InputID;
+
+    private float horizontalInput;
+    private float forwardInput;
+
+    private void Update()
+    {
+        // Get player input
+        horizontalInput = Input.GetAxis("Horizontal" + InputID);
+        forwardInput = Input.GetAxis("Vertical" + InputID);
+
+        // Move vehicle
+        transform.Translate(Vector3.forward * Time.deltaTime * carSpeed * forwardInput);
+        // Turn the vehicle left and right
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput * forwardInput);
+    }
+}
